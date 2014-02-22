faf7449d5ccfb488c0222c527dfbeee3191b42a2
Angel Ramirez <angel@cuemby.com>
2014-02-22 17:42:31 -0500

    testing twilio

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var usergrid = require('usergrid');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
